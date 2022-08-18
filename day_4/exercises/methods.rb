63252f3536ee57fd5b2131ec6a690ab32bb7507f
Amanda Ross <amikaross@gmail.com>
2022-08-18 14:42:43 -0600

    Add day 4 work

--- day_4/exercises/methods.rb
+++ day_4/exercises/methods.rb
@@ -12,16 +12,26 @@ print_name
 
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
+  p "#{name}"
 end
 
 print_name("Albus Dumbledore")
 
-# Write a method that takes in 2 numbers as arguments and prints 
+# Write a method that takes in 2 numbers as arguments and prints
 # their sum. Then call your method:
-# YOUR CODE HERE
+def add(num1, num2)
+  sum = num1 + num2
+  p sum
+end
 
-# Write a method that takes in two strings as arguments and prints 
-# a concatenation of those two strings. Example: The arguments could be 
-# (man, woman) and the end result might output: "When Harry Met Sally".  
+add(2000, 22)
+
+# Write a method that takes in two strings as arguments and prints
+# a concatenation of those two strings. Example: The arguments could be
+# (man, woman) and the end result might output: "When Harry Met Sally".
 # Then call your method:
+def concat(str1, str2)
+  p "I love both " + str1 + " and " + str2 + "."
+end
+
+concat("Lee", "Haku")
--- day_4/questions.md
+++ day_4/questions.md
@@ -2,10 +2,24 @@
 
 1. In your own words, what is the purpose of a method?
 
+To store some code in order to run use it in a variety of contexts.
+
 1. Create a method named `hello` that will print `"Sam I am"`.
+
+   * ```def hello
+          puts "Sam I am"
+        end```
 
 1. Create a method named `hello_someone` that takes an argument of `name` and prints `"#{name} I am"`.
 
+   * ```def hello_someone(name)
+          puts "#{name} I am"
+        end```
+
 1. How would you call or execute the method that you created above?
 
+   * `hello_someone("Haku")`
+
 1. What questions do you have about methods in Ruby?
+
+   * If it's all about objects.. Seems like methods have to be called on something. Is there something in main that a method without a call-ee is being called on? Some sneaky thing? 
